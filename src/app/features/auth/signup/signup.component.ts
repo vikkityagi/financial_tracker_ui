c147f338f0bcd8150e8084a1e1876d9b5f328a6f
Hema <Hema@gmail.com>
2024-09-06 17:29:40 +0530

    signup done

--- src/app/features/auth/signup/signup.component.ts
+++ src/app/features/auth/signup/signup.component.ts
@@ -1,5 +1,7 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/service/auth.service';
 
 @Component({
   selector: 'app-signup',
@@ -10,22 +12,38 @@ export class SignupComponent {
 
   signupForm!: FormGroup;
 
-  constructor(private _fb:FormBuilder){
+  constructor(private _fb:FormBuilder,private authService: AuthService){
     this.init();
   }
 
   init(){
     this.signupForm = this._fb.group({
-      username: [''],
-      email: [''],
-      password: [''],
-      confirmPassword: ['']
+      username: ['',[Validators.required]],
+      email: ['',[Validators.required]],
+      password: ['',[Validators.required]],
+      confirmPassword: ['',[Validators.required]]
     })
   }
 
   onSubmit(): void {
     if (this.signupForm.valid) {
-      console.log('Form Submitted', this.signupForm.value);
+      this.authService.signup(this.signupForm.value).subscribe({
+        next: data=>{
+          if(data.status === 201){
+            this.signupForm.reset();
+          }else if(data.status === 200){
+            alert("Please try again..")
+          }
+        },
+        error: (err: HttpErrorResponse)=>{
+          if(err.status === 400){
+            alert("User already Registered...")
+            return
+          }
+          alert(err.message)
+        }
+      })
+      
     }
   }
 
--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,13 +1,16 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { catchError, Observable, throwError } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  private apiUrl = 'http://localhost:8081/api/auth'; // Adjust URL to match your backend
+  private apiUrl = environment.baseUrl;
+
+  
 
   constructor(private http: HttpClient) { }
 
@@ -16,6 +19,9 @@ export class AuthService {
   }
 
   signup(user: any): Observable<any> {
-    return this.http.post(`${this.apiUrl}/signup`, user);
+    return this.http.post(`${this.apiUrl}/api/users/register`, user,{observe: 'response'}).pipe(catchError((err:HttpErrorResponse)=>{
+      console.error(err);
+      return throwError(() => err);
+    }));
   }
 }
